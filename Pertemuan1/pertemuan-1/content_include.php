b57297f3337472335d5e9cc0263381c5a1168256
RyanXSR <114055331+RyanXSR@users.noreply.github.com>
2022-09-23 11:09:37 +0700

    include

---
+++ Pertemuan1/pertemuan-1/content_include.php
@@ -0,0 +1,5 @@
+<div class="content">
+    <br/>Home / About Us
+    <h1>Tentang Saya</h1>
+    <p>Nama Saya Ryan, Dari SMK N 5 Surakarta.</p>
+</div>
---
+++ Pertemuan1/pertemuan-1/header_include.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Latihan Mandiri</tilte>
+    </head>
+    <body>
+        <div class="header">
+            <h1>Ryan Eka Pratama</h1>
+            <i>Latihan include & require</i>
+            <hr/>
+        </div>
